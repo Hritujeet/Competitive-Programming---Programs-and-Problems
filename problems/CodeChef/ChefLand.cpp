0b1a14fcb322184562a28f7e421b36058d117e25
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2026-04-25 20:29:26 +0530

    ChefLand

--- problems/CodeChef/ChefLand.cpp
+++ problems/CodeChef/ChefLand.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 #define REP(i, j, k) for (int i = j; i < k; i++)
-#define v vector<float>
+#define vp vector<pair<int, int>>
 #define PB push_back
 
 using namespace std;
 
-int main(){
+void solve(vp s, int t)
+{
+    for (int i = 0; i < t; i++)
+    {
+        int subscriptions = (s[i].first + 5) / 6;
+        cout << (long long)s[i].second * subscriptions << "\n";
+    }
+}
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
+    int t;
+    cin >> t;
+
+    vp s;
+    for (int i = 0; i < t; i++)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        s.PB(make_pair(n, x));
+    }
+
+    solve(s, t);
+
     return 0;
 }
