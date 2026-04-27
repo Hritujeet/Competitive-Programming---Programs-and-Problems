d612e9ddd0530e7aa6b5f53474311bd04bb1f8b8
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2026-04-27 16:24:53 +0530

    Domino Pilling

---
+++ problems/Codeforces/DominoPilling.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+typedef long long ll;
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll m, n;
+    cin >> m >> n;
+
+    ll d = (m*n)/2;
+    cout << d;
+    return 0;
+}
